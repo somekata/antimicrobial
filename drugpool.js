7919f9714acb3f81b36e2eaab70d7259e74102a3
SOMEKATA Shiro <88067020+somekata@users.noreply.github.com>
2025-09-24 15:01:17 +0900

    Add files via upload

--- drugpool.js
+++ drugpool.js
@@ -1,11 +1,11 @@
 const mechanism = ["細胞壁合成阻害","蛋白合成阻害","DNA合成阻害","細胞膜傷害","外膜傷害","その他"];
 
 const class1 = ["抗菌薬","抗真菌薬"];
-const antibioticClass = ["βラクタム系薬","キノロン系薬","マクロライド系薬","テトラサイクリン系薬","アミノグリコシド系薬","グリコペプチド系薬","オキサゾリジノン系薬","リポペプチド系薬","ポリペプチド系薬","グリシルサイクリン系薬"];
+const antibioticClass = ["β-ラクタム系薬","キノロン系薬","マクロライド系薬","テトラサイクリン系薬","アミノグリコシド系薬","グリコペプチド系薬","オキサゾリジノン系薬","リポペプチド系薬","ポリペプチド系薬","グリシルサイクリン系薬"];
 const antifungalClass = ["アゾール系薬","キャンディン系薬","ポリエン系薬"];
 const blactums = ["ペニシリン系薬","セフェム系薬","カルバペネム系薬","ペネム系薬","モノバクタム系薬","β-ラクタマーゼ阻害剤配合"];
 const penicillins = ["古典的ペニシリン系薬","アミノペニシリン系薬","ペニシリナーゼ耐性ペニシリン系薬","抗緑膿菌作用のあるペニシリン系薬"];
-const cephems = ["セファロスポリン系薬（第一世代）","セファロスポリン系薬（第二世代）","セファロスポリン系薬（第三世代・抗緑膿菌作用なし）","セファロスポリン系薬（第三世代・抗緑膿菌作用あり）","セファロスポリン系薬（第四世代）","セファロスポリン系薬（未分類）","セファマイシン系薬","オキサセフェム系薬","セファロスポリン系薬"];
+const cephems = ["セファロスポリン系薬（第一世代）","セファロスポリン系薬（第二世代）","セファロスポリン系薬（第三世代・抗緑膿菌作用なし）","セファロスポリン系薬（第三世代・抗緑膿菌作用あり）","セファロスポリン系薬（第四世代）","セファロスポリン系薬（未分類）","セファマイシン系薬","オキサセフェム系薬"];
 const macrolides = ["14員環","15員環"]
 
 const route = ["経口", "筋肉注射", "静脈内注射","その他"];
@@ -49,7 +49,7 @@ const data = [
     name: ["アンピシリン","Ampicillin"],
     abbreviation:["ABPC","AMP"],
     original:["ビクシリン"],
-    categories: ["抗菌薬", "βラクタム系薬", "ペニシリン系薬", "アミノペニシリン系薬"],
+    categories: ["抗菌薬", "β-ラクタム系薬", "ペニシリン系薬", "アミノペニシリン系薬"],
     specifiedCategories: [],
     mechanism: mechanism[0],
     route: [route[0], route[1], route[2]],
@@ -363,7 +363,7 @@ const data = [
     specifiedCategories: [drugSpecifiedCategory[0],drugSpecifiedCategory[1],drugSpecifiedCategory[3]],
     mechanism: mechanism[0],
     route: [route[2]],
-    spectrum: parseRange("5-9,11,15", spectrumBacteria),
+    spectrum: parseRange("5-9,11,13,15", spectrumBacteria),
     detail: [
       "鉄イオンを利用して細菌内に取り込まれる新規のセファロスポリン系薬である。",
       "準備中",
@@ -379,7 +379,7 @@ const data = [
     aware: [aware[2]] // Reserve
   },   
   {
-    name: ["メロペネム"],
+    name: ["メロペネム","Meropenem"],
     abbreviation:["MEPM"],
     categories: [class1[0], antibioticClass[0], blactums[2],[drugSpecifiedCategory[0],drugSpecifiedCategory[1]]],
     specifiedCategories: [drugSpecifiedCategory[0],drugSpecifiedCategory[1]],
@@ -401,7 +401,7 @@ const data = [
     aware:[aware[1]]
   },
   {
-    name:["イミペネム/シラスタチン"],
+    name:["イミペネム/シラスタチン","Imipemen/Cilastatin"],
     abbreviation:["IPM/CS"],
     categories: [class1[0], antibioticClass[0], blactums[2],[drugSpecifiedCategory[0],drugSpecifiedCategory[1]]],
     specifiedCategories: [drugSpecifiedCategory[0],drugSpecifiedCategory[1]],
@@ -423,7 +423,7 @@ const data = [
     aware:[aware[1]]
   },
     {
-    name:["イミペネム/シラスタチン/レレバクタム"],
+    name:["イミペネム/シラスタチン/レレバクタム","Imipemen/Cilastatin/Relebactum"],
     abbreviation:["IPM/CS/REL"],
     categories: [class1[0], antibioticClass[0], blactums[2],[drugSpecifiedCategory[0],drugSpecifiedCategory[1],drugSpecifiedCategory[3]]],
     specifiedCategories: [drugSpecifiedCategory[0],drugSpecifiedCategory[1],drugSpecifiedCategory[3]],
@@ -467,7 +467,7 @@ const data = [
     aware:[aware[1]]
   },
   {
-    name: ["レボフロキサシン"],
+    name: ["レボフロキサシン","Levofloxacin"],
     abbreviation:["LVFX"],
     categories: [class1[0], antibioticClass[1],[drugSpecifiedCategory[0],drugSpecifiedCategory[1]]],
     specifiedCategories: [],
@@ -489,7 +489,7 @@ const data = [
     aware:[aware[1]]
   },
   {
-    name: ["エリスロマイシン"],
+    name: ["エリスロマイシン","Erythromycin"],
     abbreviation:["EM"],
     categories: [class1[0], antibioticClass[2],macrolides[0]],
     specifiedCategories: [],
@@ -511,7 +511,7 @@ const data = [
     aware:[aware[1]]
     },
   {
-    name: ["クラリスロマイシン"],
+    name: ["クラリスロマイシン","Clarithromycin"],
     abbreviation:["CAM"],
     categories: [class1[0], antibioticClass[2],macrolides[0]],
     specifiedCategories: [],
@@ -533,7 +533,7 @@ const data = [
     aware:[aware[1]]
   },
   {
-    name: ["アジスロマイシン"],
+    name: ["アジスロマイシン","Azithromycin"],
     abbreviation:["AZM"],
     categories: [class1[0], antibioticClass[2],macrolides[1]],
     specifiedCategories: [],
@@ -901,13 +901,14 @@ const data = [
     spectrum: ["カンジダ","アスペルギルス","クリプトコックス","ムーコル"],
     detail: [
       "トリアゾール系薬の中では極めてスペクトルが広い",
-      "詳細情報1:準備中",
+      "2020年1月23日に深在性真菌症の治療と予防のために承認され、同年4月8日から保険適用されている。",
       "詳細情報2:準備中",
   ],
       reference:[
+        "深在性真菌症治療剤「ノクサフィル®」の 効能又は効果の追加承認を取得"
       ],
       url:[
-        "",
+        "https://www.msd.co.jp/news/product-news-0927/",
       ],
   asc:["非該当"],
   asi:["非該当"],
@@ -923,13 +924,14 @@ const data = [
     spectrum: ["カンジダ","アスペルギルス","クリプトコックス","ムーコル"],
     detail: [
       "トリアゾール系薬の中では極めてスペクトルが広い",
-      "詳細情報1:準備中",
+      "2022年12月23日に深在性真菌症治療薬として製造販売が承認された。",
       "詳細情報2:準備中",
   ],
       reference:[
+        "医療用医薬品 : クレセンバ"
       ],
       url:[
-        "",
+        "https://www.kegg.jp/medicus-bin/japic_med?japic_code=00070735"
       ],
   asc:["非該当"],
   asi:["非該当"],
@@ -949,9 +951,10 @@ const data = [
       "詳細情報2:準備中",
   ],
       reference:[
+        
       ],
       url:[
-        "",
+        ""
       ],
   asc:["非該当"],
   asi:["非該当"],
@@ -1018,12 +1021,17 @@ function setAsc(dataArray) {
 setAsc(data);
 
 const categoriesInfo = {
+  "クイックガイド": "このページの操作方法や注意点は <a href='howtouse.html' target='_blank'>こちら</a> をご覧ください。",
   "抗菌薬": `抗菌薬は原則として細菌に作用する治療薬である。<p class='whiteback'><strong>主な抗菌薬の種類:</strong><ul class='explain'>${antibioticClass.map(item => `<li>${item}</li>`).join("")}</ul></p><p class='whiteback'><strong>その他の分類:</strong><ul class='explain'>${drugSpecifiedCategory.map(item => `<li>${item}</li>`).join("")}</ul></p>`,
-  "βラクタム系薬": `βラクタム系薬は細胞壁合成阻害を作用機序とする最大のカテゴリーである。${blactums.join("、")}に分類される。<br>　さらに、ペニシリン系薬は、${penicillins.join("、")}に、<br>　セフェム系薬は${cephems.join("、")}に分類される。<p class='whiteback'><strong>主なβラクタム系薬の種類:</strong> ${blactums.join("、")}</p>`,
-  "ペニシリン系薬": `ペニシリン系薬は、βラクタム系薬の1系統で、さらに、${penicillins.join("、")}に分類される。また、${blactums[5]}もある。`,
+  "β-ラクタム系薬": `β-ラクタム系薬は細胞壁合成阻害を作用機序とする最大のカテゴリーである。${blactums.join("、")}に分類される。<br>　さらに、ペニシリン系薬は、${penicillins.join("、")}に、<br>　セフェム系薬は、${cephems.join("、")}に分類される。<p class='whiteback'><strong>主なβ-ラクタム系薬の種類:</strong> ${blactums.join("、")}</p><p class='whiteback'>参考：<a href="http://filgap.kenkyuukai.jp/special/?id=42286" target="_blank">Fil-GAP: 各論1～β-ラクタム系薬</a></p>`,
+  "ペニシリン系薬": `ペニシリン系薬は、β-ラクタム系薬の1系統で、さらに、${penicillins.join("、")}に分類される。また、${blactums[5]}もある。<br>　ペニシリナーゼ産生のブドウ球菌用に開発されたペニシリナーゼ耐性ペニシリン系薬（メチシリン、オキサシリン）は、臨床的には使用されないため、リストからは除外したが、メチシリン耐性黄色ブドウ球菌（MRSA）の基準となっており、検査や歴史的には重要なペニシリン系薬である。`,
   "アミノペニシリン系薬": "アミノ基を持つことで、一部のグラム陰性菌にもスペクトルを広げた半合成のペニシリン系薬。",
-  "カルバペネム系薬": "カルバペネム系薬は、βラクタマーゼに安定であり、最も広域のβラクタム系薬である。広域抗菌薬の代表でもある。",
-  "セフェム系薬": `${blactums[1]}は、ペニシリナーゼに安定であり、スペクトルはおおむね${blactums[0]}と${blactums[2]}の中間的な位置づけである。ただし、世代や系統などによってスペクトルの広さが異なる。${parseRange("8", cephems)}、${parseRange("6", cephems)}、${parseRange("7", cephems)}}に分類され、${parseRange("8", cephems)}はさらに、第一世代から第四世代*までに分類される。世代未分類として、${data[9].name[0]}がある。<br>*海外では、抗MRSA薬として第五世代に分けることがある。`,
+  "カルバペネム系薬": "カルバペネム系薬は、β-ラクタマーゼに安定であり、最も広域のβ-ラクタム系薬である。広域抗菌薬の代表でもある。",
+  "セフェム系薬": `${blactums[1]}は、ペニシリナーゼに安定であり、スペクトルはおおむね${blactums[0]}と${blactums[2]}の中間的な位置づけである。ただし、世代や系統などによってスペクトルの広さが異なる。${parseRange("8", cephems)}、${parseRange("6", cephems)}、${parseRange("7", cephems)}に分類される。
+  <p class='whiteback'>　セファロスポリン系薬は、さらに、第一世代から第四世代*までに分類される。世代未分類として、${data[13].name[0]}がある。一般に、ペニシリン系薬に比べてスペクトルは広いが、嫌気性菌、リステリア、腸球菌に対してはペニシリンに劣る場合がある。世代が大きくなるにつれて概してグラム陰性菌へのスペクトルが広がる。また、第三世代セファロスポリン系薬は、抗緑膿菌作用のないセフォタキシムやセフトリアキソン、抗緑膿菌作用のあるセフタジジムに分類される。セフォタキシムやセフトリアキソンはグラム陽性菌にも第一世代と同程度に有効であるが、セフタジジムはグラム陽性菌に対する抗菌力が劣る。第四世代はグラム陽性菌にもグラム陰性菌にも幅広く抗菌力を示す。<br>*海外では、抗MRSA薬として第五世代に分けることがある。</p>
+  <p class='whiteback'>　セファマイシン系薬の最も大きな特徴は、セファロスポリン系薬が苦手とする嫌気性菌に対する抗菌力である。また、基質特異性拡張型β-ラクタマーゼに対しても、セファロスポリン系薬よりも安定である。</p>
+  <p class='whiteback'>参考：<a href="http://filgap.kenkyuukai.jp/special/?id=42822" target="_blank">Fil-GAP: 各論1B～セフェム系薬</a></p>`,
+  "セファロスポリン系薬": `<p class='whiteback'>参考：<a href="http://filgap.kenkyuukai.jp/special/?id=42822" target="_blank">Fil-GAP: 各論1B～セフェム系薬</a></p>`,
   "第三世代セファロスポリン系薬": "第三世代セファロスポリン系薬は、比較的スペクトルは広いが、スペクトルがグラム陽性よりの抗緑膿菌作用のない薬剤とグラム陰性菌よりの抗緑膿菌作用のある薬剤に分類される。前者の代表がセフトリアキソン、後者の代表がセフタジジムである。",
   "特定抗菌薬":"「適正使用のために特に定めた抗菌薬」というニュアンスで用いられるが、明確な定義はない。ただし、一般に、広域抗菌薬、抗MRSA薬を含む。それ以外に、コリスチンやチゲサイクリンのような多剤耐性菌に使用される特殊な抗菌薬を含む。",
   "抗MRSA薬": "メチシリン耐性黄色ブドウ球菌感染症に対する治療薬の総称で、4系統6薬剤が知られている。",
@@ -1040,29 +1048,11 @@ const categoriesInfo = {
   "トリアゾール系薬": "トリアゾール系薬は、エルゴステロール合成阻害を作用機序とする抗真菌薬である。代表的な薬剤として、フルコナゾール、イトラコナゾール、ボリコナゾール、ポサコナゾール、イサブコナゾールがある。",
   "ポリエン系薬": "ポリエン系薬は、細胞膜傷害を作用機序とする抗真菌薬である。代表的な薬剤として、アムホテリシンBがある。",
   "キャンディン系薬": "キャンディン系薬は、細胞壁合成阻害を作用機序とする抗真菌薬である。代表的な薬剤として、ミカファンギン、カスポファンギンがある。",
-  "AWaRe分類とは":"AWaRe分類は、以下で説明する、Access、Watch、Reserveの頭文字をとったもので、抗菌薬の適正使用を促進するための分類システムである。<br><a href='https://amrcrc.ncgm.go.jp/surveillance/020/AWaRe_excel_2019_ver.4.1.xlsx' target='_blank'>AWaRe分類リスト-抗菌薬種類別</a><ul class='explain'><li>Access（アクセス）：広く使用される抗菌薬で、効果的かつ安全とされている。</li><li>Watch（ウォッチ）：耐性リスクが高いため、慎重に使用すべき抗菌薬である。</li><li>Reserve（リザーブ）：多剤耐性病原体に対してのみ使用すべき、最後の手段となる抗菌薬である。</li></ul>",
-  "DASCとは":"DASC（Days of Antibiotic Spectrum Coverage）は、ASC（Antibiotic Spectrum Coverage, その抗菌薬がカバーするスペクトル）*1の消費日数を指す。<ol>参考文献<li>Kakiuchi S et al. Days of Antibiotic Spectrum Coverage: A Novel Metric for Inpatient Antibiotic Consumption. Clin Infect Dis. 2022 Sep10;75(4):567-576.</li></ol>",
-  "ASIとは":"ASI（Antibiotic Spectrum Index）は、抗菌薬ごとのスペクトルの広さをスコア化したものである。病院間比較や研究における抗菌薬使用パターンのベンチマークとして開発された。<ol>参考文献<li>https://www.gpas-online.org/antibiotic-spectrum-index/</li><li>Zombori L et al. Antibiotic spectrum index as an antimicrobial stewardship tool in paediatric intensive care settings. Int J Antimicrob Agents. 2023 Feb;61(2):106710.</li><li>Gerber JS et al. Development and Application of an Antibiotic Spectrum Index for Benchmarking Antibiotic Selection Patterns Across Hospitals. Infect Control Hosp Epidemiol. 2017 Aug;38(8):993-997.</li></ol>",
+  "AWaRe分類":`AWaRe分類は、以下で説明する、Access、Watch、Reserveの頭文字をとったもので、抗菌薬の適正使用を促進するための分類システムである。<br><a href='https://amrcrc.ncgm.go.jp/surveillance/020/AWaRe_excel_2019_ver.4.1.xlsx' target='_blank'>AWaRe分類リスト-抗菌薬種類別</a><ul class='explain'><li>Access（アクセス）：広く使用される抗菌薬で、効果的かつ安全とされている。</li><li>Watch（ウォッチ）：耐性リスクが高いため、慎重に使用すべき抗菌薬である。</li><li>Reserve（リザーブ）：多剤耐性病原体に対してのみ使用すべき、最後の手段となる抗菌薬である。</li></ul>
+  <p class='whiteback'>参考：<a href="http://filgap.kenkyuukai.jp/special/?id=43178" target="_blank">Fil-GAP: 抗菌薬　実践編2</a></p>`,
+  "DASC":"DASC（Days of Antibiotic Spectrum Coverage）は、ASC（Antibiotic Spectrum Coverage, その抗菌薬がカバーするスペクトル）*1の消費日数を指す。<ol>参考文献<li>Kakiuchi S et al. Days of Antibiotic Spectrum Coverage: A Novel Metric for Inpatient Antibiotic Consumption. Clin Infect Dis. 2022 Sep10;75(4):567-576.</li></ol>",
+  "ASI":"ASI（Antibiotic Spectrum Index）は、抗菌薬ごとのスペクトルの広さをスコア化したものである。病院間比較や研究における抗菌薬使用パターンのベンチマークとして開発された。<ol>参考文献<li>https://www.gpas-online.org/antibiotic-spectrum-index/</li><li>Zombori L et al. Antibiotic spectrum index as an antimicrobial stewardship tool in paediatric intensive care settings. Int J Antimicrob Agents. 2023 Feb;61(2):106710.</li><li>Gerber JS et al. Development and Application of an Antibiotic Spectrum Index for Benchmarking Antibiotic Selection Patterns Across Hospitals. Infect Control Hosp Epidemiol. 2017 Aug;38(8):993-997.</li></ol>",
+  "ESBL":"基質特異性拡張型β-ラクタマーゼ（extended spectrume beta-lactamase）の略。元々は、ペニシリン系薬を加水分解していたβ-ラクタマーゼが突然変異により第三世代セファロスポリンなども加水分解できるようになったもの（TEM系・SHV系）を指した。現在主流のCTX-M系は起源が異なるが、第三世代セファロスポリンに対する活性と阻害剤感受性の点でESBLに含まれる。実務上は「第三世代セファロスポリンおよびアズトレオナムを加水分解し、クラブラン酸等の阻害剤で阻害されるAmbler分類 Class Aのβ-ラクタマーゼ（原則としてセファマイシンとカルバペネムは加水分解しない）」と理解する。ESBL産生菌の多くは腸内細菌目細菌で、臨床的に問題となるのは主に大腸菌と肺炎桿菌である。",
+  "MBL":"メタロβ-ラクタマーゼ（metallo beta-lactamase）の略。Ambler分類Class Bに属する金属依存性のβ-ラクタマーゼで、活性中心に亜鉛イオンを有する。ペニシリン系・セファロスポリン系・カルバペネム系を広く加水分解し、従来の阻害剤（クラブラン酸、タゾバクタム、アビバクタム等）では阻害されない。一方、モノバクタム系（アズトレオナム）は MBL自体では通常加水分解されにくいが、ESBLやAmpCを同時に産生する場合、臨床的には耐性となることがある。代表的なMBLにはIMP型、VIM型、NDM型があり、日本のCPE（carbapenemase-producing <i>Enterobacterales</i>）では IMP-1、IMP-6 が比較的多い。金属キレート剤（例：EDTA）で活性が低下するのが特徴である。",
+  "DTR":`治療困難な耐性（difficult to treat）の略。米国感染症学会（IDSA）が提唱・普及させた、主として腸内細菌科、緑膿菌、アシネトバクターなどグラム陰性桿菌で用いられる臨床的な耐性表現型である。CREやMDRのような微生物学的カテゴリーではなく、“第一選択として使いたい薬が使えない”という治療上の困難さを直接表現するのが特徴である。詳細はIDSAガイドラインを参照。<p class='whiteback'>参考：<a href="https://www.idsociety.org/practice-guideline/amr-guidance/" target="_blank">IDSA 2024 Guidance on the Treatment of Antimicrobial Resistant Gram-Negative Infections</a></p>`
 };
-
-/*
-const antimicrobials = [
-  { name: ["アンピシリン", "Ampicillin"], abbreviation:["ABPC","AMP"],class:[class1[0],class2[0],class3[0],class4[0]],original:["ビクシリン"],application:[],spectrum:["PSSP","<i>Esherrichia coli</i>","BLNAS",""],route:["経口","筋肉注射/静脈内注射"],dose:["250～500mgを1日4〜6回","(成人)<br>筋肉内注射の場合: 1回250〜1000mg（力価）を1日2〜4回<br>静脈内注射の場合: 1日量1〜2g（力価）を1〜2回静脈内注射または1日量1〜4g（力価）を1〜2回に分けて点滴注射<br>(小児)1日100〜200mg（力価）/kgを3〜4回に分けて静脈内注射または点滴注射。投与量の上限は1日400mg（力価）/kgまで。<br>(新生児)1日50〜200mg（力価）/kgを2〜4回に分けて静脈内注射または点滴静注。"],adme:[],ref:["インタビューフォーム"],url:["https://www.kegg.jp/medicus-bin/japic_med?japic_code=00057170"],asc: 5, classification: "Access" },
-  { name: ["スルバクタム/アンピシリン", "Sulbactum/Ampicillin"], abbreviation:["ABPC","AMP"],asc: 8, classification: "Access" },
-  { name: ["ピペラシリン", "PIPC"], asc: 7, classification: "Watch" },
-  { name: ["タゾバクタム/ピペラシリン", "TAZ/PIPC"], asc: 11, classification: "Watch" },
-  { name: ["セファゾリン", "CEZ"], asc: 3, classification: "Access" },
-  { name: ["セフトリアキソン", "CTRX"], asc: 6, classification: "Watch" },
-  { name: ["セフタジジム", "CAZ"], asc: 6, classification: "Watch" },
-  { name: ["セフェピム", "CFPM"], asc: 8, classification: "Watch" },
-  { name: ["メロペネム", "MEPM"], asc: 12, classification: "Watch" },
-  { name: ["バンコマイシン", "VCM"], asc: 5, classification: "Watch" },
-  { name: ["リネゾリド", "LZD"], asc: 6, classification: "Reserve" },
-  { name: ["テジゾリド", "TZD"], asc: 6, classification: "Reserve" },
-  { name: ["ダプトマイシン", "DAP"], asc: 6, classification: "Reserve" },
-  { name: ["シプロフロキサシン", "CPFX"], asc: 9, classification: "Watch" },
-  { name: ["アジスロマイシン", "AZM"], asc: 6, classification: "Watch" },
-  { name: ["コリスチン", "COL"], asc: 6, classification: "Reserve" },
-  { name: ["メトロニダゾール", "MTZ"], asc: 2, classification: "Access" },
-];
-*/
